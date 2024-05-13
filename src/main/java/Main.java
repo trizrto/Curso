5ab1b900553758d3f324234a9c525fd3d9419d3d
Beatriz Huberto <169067859+trizrto@users.noreply.github.com>
2024-05-13 22:56:25 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("   =================");
+    System.out.println("   = Curso técnico =");
+    System.out.println("   =================");
+    System.out.println("");
+    System.out.println(" > Desenvolvimento de sistemas  < ");
+    System.out.println("");
+    System.out.println(" No começo do ano confesso que o curso não me agradou, mas quando vimos tabela verdade melhorou, achei muito interessante.");
+    System.out.println(" A metodologia de provas me agrada, pois foge do tradicional e simplifica a realização das mesmas.");
+    System.out.println(" O curso ajuda a ter um pensamento lógico por conta dos exercícios. Tais como números binários, java e tabela verdade.");
+    System.out.println(" O java é muito complicado, mas não é um bicho de sete cabeças. Porém eu gostaria que fosse phyton pelos códigos mais simples e rápidos, sendo uma boa linguagem para iniciantes na programação.");
+  }
+
+
+}
